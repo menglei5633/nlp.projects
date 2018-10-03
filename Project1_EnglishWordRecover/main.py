be2f677e7d07b38462fc153146de351b93c2c854
menglei5633 <menglei5633@163.com>
2018-10-03 13:44:24 +0800

    add read irrgular verb and none

--- Project1_EnglishWordRecover/main.py
+++ Project1_EnglishWordRecover/main.py
@@ -14,8 +14,55 @@ def read_dic(filename):
     return dic
     pass
 
-dic = read_dic("dic_ec.txt")
+def read_BuguizeVerb(filename):
+    fileHandle = open(filename, "r")
+    buguizeVerb = {}
+    lineList = fileHandle.readlines()
+    for line in lineList:
+        #print(line)
+        wordList = line.split(" ")
+        guoqushiList = wordList[1].split(",")
+        guoqufenciList = wordList[2].split(",")
+        for word in guoqushiList:
+            buguizeVerb[word] = wordList[0]
+            pass
+        for word in guoqufenciList:
+            buguizeVerb[word] = wordList[0]
+            pass
+        pass
+    return buguizeVerb
+    pass
+
+def read_buguizeNone(filename):
+    fileHandle = open(filename, "r")
+    lineList = fileHandle.readlines()
+    dic = {}
+    for line in lineList:
+        #print(line)
+        wordList = line.split(" ")
+        '''
+        for i in range(len(wordList)):
+            print(wordList[i])
+            pass
+        print("______")
+        '''
+        dic[wordList[1]] = wordList[0]
+        pass
+    return dic
+    pass
+
+#dic = read_dic("dic_ec.txt")
 #for name in dic:
 #    print( name +" " + dic[name][0] + " " + dic[name][1])
 #    pass
+#buguizeVerb = read_BuguizeVerb("IrregularVerbList")
+#for name in buguizeVerb:
+#    print( "key: " + name + " value: " + buguizeVerb[name])
+#    pass
+
+buguizeNone = read_buguizeNone("IrregularPluralNouns")
+for name in buguizeNone:
+    print("key: " + name + " value: " + buguizeNone[name])
+    pass
+
 
